16d9bd396043534cd82b28edf2215267526b370e
NicoleRöhm <donjay1014@gmail.com>
2023-06-08 11:40:00 +0200

    Initial Commit

---
+++ newProjekt/newProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  newProjektApp.swift
+//  newProjekt
+//
+//  Created by Nicole Röhm on 08.06.23.
+//
+
+import SwiftUI
+
+@main
+struct newProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
