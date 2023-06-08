719a0558327bdf3f8e2e2f17483a496740073a25
NicoleRöhm <donjay1014@gmail.com>
2023-06-08 11:47:04 +0200

    my results

---
+++ newProjekt/View/AddRezeptView.swift
@@ -0,0 +1,20 @@
+//
+//  AddRezeptView.swift
+//  newProjekt
+//
+//  Created by Nicole Röhm on 08.06.23.
+//
+
+import SwiftUI
+
+struct AddRezeptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRezeptView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRezeptView()
+    }
+}
---
+++ newProjekt/ViewModel/RezeptViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RezeptViewModel.swift
+//  newProjekt
+//
+//  Created by Nicole Röhm on 08.06.23.
+//
+
+import Foundation
