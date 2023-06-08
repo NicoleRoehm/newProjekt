325a65432fe258ec7f50133b28ae820c47873ef0
NicoleRöhm <donjay1014@gmail.com>
2023-06-08 12:31:58 +0200

    my results

--- newProjekt/ViewModel/RezeptViewModel.swift
+++ newProjekt/ViewModel/RezeptViewModel.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import CoreData
+
+class RezeptViewModel : ObservableObject{
+    
+    let container = NSPersistentContainer(name: "Rezepte")
+    
+    @Published var rezeptList : [Rezept] = []
+    
+    init() {
+        
+        container.loadPersistentStores{ _, error in
+            if let error = error {
+                fatalError("core data store faild: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func fetchRezepte(){
+        
+        let request = NSFetchRequest<Rezept>(entityName: String(describing: Rezept.self))
+        
+        do{
+            rezeptList = try container.viewContext.fetch(request)
+        }catch {
+            print("loading the data failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func createRezept(rezeptName: String, portionen: Int16, zutaten: String, beschreibung: String){
+        
+        
+    }
+    
+    func updateRezept(_ rezept: Rezept, rezeptName: String, portionen: Int16, zutaten: String, beschreibung: String){
+        
+        
+    }
+    
+    func deletRezept(indexSet: IndexSet){
+        
+        
+    }
+    
+    func fetchRezeptbyRezeptName(){
+        
+    }
+}
+    
+
+
